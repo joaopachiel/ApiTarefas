abfde0da725fb0374ec40bd63187f63d1bcb2cd4
joaopachiel <joaopachiel@gmail.com>
2023-06-21 11:15:31 -0300

    Criação de UsuarioController.

---
+++ ApiTarefas/Controllers/UsuarioController.cs
@@ -0,0 +1,17 @@
+﻿using ApiTarefas.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiTarefas.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<List<UsuarioModel>> BuscarTodosUsuarios()
+        {
+            return Ok();
+        }
+    }
+}
