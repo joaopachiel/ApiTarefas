f4d625062be964a6bf1a9236f13a1b28c99b4f78
joaopachiel <joaopachiel@gmail.com>
2023-06-21 11:56:23 -0300

    Criação e configuração do context.

---
+++ ApiTarefas/Data/ApiTarefasContext.cs
@@ -0,0 +1,22 @@
+﻿using ApiTarefas.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiTarefas.Data
+{
+    public class ApiTarefasContext : DbContext
+    {
+        public ApiTarefasContext(DbContextOptions<ApiTarefasContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<UsuarioModel> Usuarios { get; set; }
+        public DbSet<TarefaModel> Tarefas { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {   
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
