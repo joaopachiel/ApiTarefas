88fe9ff56104e52cf1ad2333754a1dbb4a1cb378
joaopachiel <joaopachiel@gmail.com>
2023-08-14 11:39:17 -0300

    Crud UsuarioController.

--- ApiTarefas/Controllers/UsuarioController.cs
+++ ApiTarefas/Controllers/UsuarioController.cs
@@ -1,4 +1,5 @@
 ﻿using ApiTarefas.Models;
+using ApiTarefas.Repositorios.Interfaces;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -8,10 +9,48 @@ namespace ApiTarefas.Controllers
     [ApiController]
     public class UsuarioController : ControllerBase
     {
-        [HttpGet]
-        public ActionResult<List<UsuarioModel>> BuscarTodosUsuarios()
+        private readonly IUsuarioRepositorio _usuarioRepositirio;
+        public UsuarioController(IUsuarioRepositorio usuarioRepositirio)
         {
-            return Ok();
+            _usuarioRepositirio = usuarioRepositirio;
         }
+
+
+        [HttpGet]
+        public async Task<ActionResult<List<UsuarioModel>>> BuscarTodosUsuarios()
+        {
+            List<UsuarioModel> usuarios = await _usuarioRepositirio.BuscarTodosUsuarios();
+            return Ok(usuarios);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<List<UsuarioModel>>> BuscarPorId(int id)
+        {
+            UsuarioModel usuario = await _usuarioRepositirio.BuscarPorId(id);
+            return Ok(usuario);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<UsuarioModel>> Cadastrar([FromBody] UsuarioModel usuarioModel)
+        {
+            UsuarioModel usuario = await _usuarioRepositirio.Adicionar(usuarioModel);
+            return Ok(usuario);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<UsuarioModel>> Atualizar([FromBody] UsuarioModel usuarioModel, int id)
+        {
+            usuarioModel.Id = id;
+            UsuarioModel usuario = await _usuarioRepositirio.Atualizar(usuarioModel, id);
+            return Ok(usuario);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<UsuarioModel>> Apagar(int id)
+        {
+            bool apagado = await _usuarioRepositirio.Apagar(id);
+            return Ok(apagado);
+        }
+
     }
 }
