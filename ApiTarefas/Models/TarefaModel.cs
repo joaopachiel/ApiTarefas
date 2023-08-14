d085804bd7e465c16cca8d7220f8c7ece3425a31
joaopachiel <joaopachiel@gmail.com>
2023-08-14 11:36:44 -0300

    Criação do Enum de status de tarefa.

--- ApiTarefas/Models/TarefaModel.cs
+++ ApiTarefas/Models/TarefaModel.cs
@@ -1,10 +1,12 @@
-﻿namespace ApiTarefas.Models
+﻿using ApiTarefas.Enums;
+
+namespace ApiTarefas.Models
 {
     public class TarefaModel
     {
         public int Id { get; set; }
         public string? Nome { get; set; }
         public string? Descricao { get; set; }
-        public int Status { get; set; }
+        public StatusTarefa Status { get; set; }
     }
 }
