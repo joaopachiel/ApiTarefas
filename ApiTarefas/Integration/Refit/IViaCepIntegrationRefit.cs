10604210bff1f95ace1c8d5b0b87887ad46e2d3b
joaopachiel <joaopachiel@gmail.com>
2023-08-16 15:29:21 -0300

    feat: criação da interface que vai fazer as requisições http para o serviço ViaCep.

---
+++ ApiTarefas/Integration/Refit/IViaCepIntegrationRefit.cs
@@ -0,0 +1,11 @@
+﻿using ApiTarefas.Integration.Response;
+using Refit;
+
+namespace ApiTarefas.Integration.Refit
+{
+    public interface IViaCepIntegrationRefit
+    {
+        [Get("/ws/{cep}/json")]
+        Task<ApiResponse<ViaCepResponse>> ObterDadosViaCep(string cep);
+    }
+}
