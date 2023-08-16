bf6aca9773f1a3bae735d18d82066e662e23862a
joaopachiel <joaopachiel@gmail.com>
2023-08-16 15:50:40 -0300

    feat: Injeção de denendência e método get de listar dados do endereço.

---
+++ ApiTarefas/Controllers/CepController.cs
@@ -0,0 +1,30 @@
+﻿using ApiTarefas.Integration.Interfaces;
+using ApiTarefas.Integration.Response;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiTarefas.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CepController : ControllerBase
+    {
+        private readonly IViaCepIntegration _viaCepIntegration;
+
+        public CepController(IViaCepIntegration viaCepIntegration)
+        {
+            _viaCepIntegration = viaCepIntegration;
+        }
+        [HttpGet("{cep}")]
+        public async Task<ActionResult<ViaCepResponse>> ListarDadosEndereco(string cep)
+        {
+            var responseData = await _viaCepIntegration.ObterDadosViaCep(cep);
+
+            if(responseData == null)
+            {
+                return BadRequest("Cep não encontrado!");
+            }
+            return Ok(responseData);
+        }
+    }
+}
